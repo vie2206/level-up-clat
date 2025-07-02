81e68a25d38cbd22f2ec1b1c35bb07bdf363f855
vie2206 <official@legalight.in>
2025-07-02 22:41:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files
 app.use(express.static('.'));
 
-// Route for main page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`🚀 LEVEL UP running on port ${PORT}`);
+    console.log('🚀 LEVEL UP running on port ' + PORT);
 });
